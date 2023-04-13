3260164ffec3710920c3cf65db5211524c5da5f2
Nikita Yagovkin <cvininka@mail.ru>
2023-04-14 00:08:29 +0500

    gen student

---
+++ HomeSol/Home_5/Program.cs
@@ -0,0 +1,51 @@
+﻿using Home_5;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stGroup = 1;
+
+            Student[] group = new Student[5];
+            for (int i = 0; i <= 4; i++)
+            {
+                int num = i;
+                int id = 100 + i;
+                Random random = new Random();
+                string name = Convert.ToString(random.NextInt64(100, 200));
+                Student student = new Student(id, name, (int)random.NextInt64(17, 25), stGroup);
+                group[num] = student;
+                student.InfoSt();
+            }
+
+            stGroup++;
+            Student[] group2 = new Student[5];
+            for (int i = 0; i <= 4; i++)
+            {
+                int num = i;
+                int id = 200 + i;
+                Random random = new Random();
+                string name = Convert.ToString(random.NextInt64(100, 200));
+                Student student = new Student(id, name, (int)random.NextInt64(17, 25), stGroup);
+                group2[num] = student;
+                student.InfoSt();
+            }
+
+            stGroup++;
+            Student[] group3 = new Student[5];
+            for (int i = 0; i <= 4; i++)
+            {
+                int num = i;
+                int id = 300 + i;
+                Random random = new Random();
+                string name = Convert.ToString(random.NextInt64(100, 200));
+                Student student = new Student(id, name, (int)random.NextInt64(17, 25), stGroup);
+                group3[num] = student;
+                student.InfoSt();
+            }
+        }
+    }
+}
---
+++ HomeSol/Home_5/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_5
+{
+    public class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+        public int group;
+        public int mathMark;
+        public int physicalEducationMark;
+        public int biologyMark;
+        public int reward = 0;
+
+        public Student(int stId, string stName, int stAge, int stGroup) 
+        { 
+            name = stName;
+            age = stAge; 
+            group = stGroup;
+            id = stId;
+            Random random = new Random();
+            mathMark = (int)random.NextInt64(1, 10);
+            physicalEducationMark = (int)random.NextInt64(1, 10);
+            biologyMark = (int)random.NextInt64(1, 10);  
+        }
+
+        public void InfoSt ()
+        {
+            Console.WriteLine($"Group: {group} d: {id} Name: {name} Age: {age}  mathMark: {mathMark} physicalEducationMark: {physicalEducationMark} biologyMark: {biologyMark} reward: {reward}");
+        }
+
+
+    }
+}
