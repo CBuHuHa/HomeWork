2b0fc046f50677e63652dc2debd4218c12d86b2c
Nikita Yagovkin <cvininka@mail.ru>
2023-04-24 21:00:35 +0500

    Home_8  v0.1

---
+++ HomeSol/Home_8/ProductCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_8
+{
+    public class ProductCreator
+    {
+        public List<Product> CreateRation(int count)
+        {
+            string[] food = { "Banana", "Ham", "Beef", "Pear", "Potato", "Shrimp", "Chicken", "Butter", "Carrot", "Tomato", "Pork", "Spaghetti", "Bread", "Chocolate" };
+            int minCal = 100;
+            int maxCal = 300;
+            List<Product> list = new List<Product>();
+
+            for (int i = 0; i < count; i++)
+            {
+                Random random = new Random();
+                Product product = new Product(food[random.Next(food.Length)].ToString(), random.Next(minCal, maxCal));
+                list.Add(product);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ HomeSol/Home_8/Program.cs
@@ -0,0 +1,39 @@
+﻿using Home_8;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCreator creator = new ProductCreator();
+
+            Dictionary<Week, List<Product>> ration = new Dictionary<Week, List<Product>>()
+            {
+                { Week.Monday, creator.CreateRation() },
+                { Week.Tuesday, creator.CreateRation() },
+                { Week.Wednesday, creator.CreateRation() },
+                { Week.Thursday, creator.CreateRation() },
+                { Week.Friday, creator.CreateRation() },
+                { Week.Saturday, creator.CreateRation() },
+                { Week.Sunday, creator.CreateRation() },
+            };
+
+            foreach (var item in ration)
+            {
+                Console.WriteLine(item.Key.ToString());
+                foreach (var product in item.Value)
+                {
+                    string name = product.Name;
+                    int cal = product.Calories;
+
+                    Console.WriteLine($" {name} : {cal}");
+                }
+            }
+
+        }
+    }
+}
